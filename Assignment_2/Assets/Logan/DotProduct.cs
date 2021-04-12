303612a6df8d6ffa3e7e36ec990d9f9f70e1bacd
LoganLevitre <45443105+LoganBEary@users.noreply.github.com>
2021-04-11 17:26:24 -0700

    added Dotproduct

---
+++ Assignment_2/Assets/Logan/DotProduct.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotProduct : MonoBehaviour
+{
+    public Transform player;
+    public Transform enemy;
+    public GameObject alertTxtObj;
+    // Update is called once per frame
+    void Start()
+    {
+      alertTxtObj.SetActive(false);
+    }
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+      Vector3 playerForward = player.forward;
+      Vector3 playerToEnemy = enemy.position - player.position;
+      float dotProduct = Vector3.Dot(playerForward, playerToEnemy);
+
+      showAlert(dotProduct);
+    //  print(dotProduct);
+    }
+    void showAlert(float dotProduct)
+    {
+      if(dotProduct <= 3.5 && dotProduct >= -2.5)
+      {
+        alertTxtObj.SetActive(true);
+      }
+      else
+      {
+        alertTxtObj.SetActive(false);
+      }
+    }
+}
